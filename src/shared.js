175875b8ca1d97aa9f9b4be533ad1e7e0a3b231b
jatinghanghas315 <jatinghanghas315@gmail.com>
2026-03-13 14:01:08 +0530

    Add 'My Wallet' link to dropdown menu

--- src/shared.js
+++ src/shared.js
@@ -406,6 +406,7 @@ function renderNav() {
           '<a href="browse.html" class="gl-dd-item" role="menuitem">🔍 Browse Gigs</a>' +
           '<a href="post-gig.html" class="gl-dd-item" role="menuitem">➕ Post a Gig</a>' +
           '<div class="gl-dd-divider"></div>' +
+          '<a href="wallet.html" class="gl-dd-item" role="menuitem">💰 My Wallet</a>' +
           '<a href="help-center.html" class="gl-dd-item" role="menuitem">❓ Help Center</a>' +
           '<button class="gl-dd-item danger" id="ddLogout" role="menuitem">🚪 Logout</button>' +
         '</div>' +
